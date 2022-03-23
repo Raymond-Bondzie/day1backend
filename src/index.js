0aa29a43c7d905dcbd76e2ff98ff2ada3cc3a908
Raymond Bondzie <raymond.bondzie.2@gmail.com>
2022-03-23 13:58:03 +0000

    requesting for file paths

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ const express = require('express')
 
 const server = express();
 
-const handleRequest = (req , res) => {
-    res.send("hello world")
+const path = require('path')
+
+// const handleRequest = (req , res) => {
+//     res.send("hello world")
+// }
+
+const handleHomeRequest = (req, res) => {
+    let homePath = path.join(__dirname, 'public', 'pages', 'home.html')
+
+    res.sendFile(homePath)
 }
 
-server.use('/', handleRequest)
+const handleAboutRequest = (req, res) => {
+    let aboutPath = path.join(__dirname, 'public', 'pages', 'about.html')
+    res.sendFile(aboutPath)
+}
+
+// server.use('/', handleRequest)
+server.get('/about', handleAboutRequest)
+ server.get("/", handleHomeRequest);
+
+
 
 server.listen("3500", () => {
     console.log('server is running on port 6000');
